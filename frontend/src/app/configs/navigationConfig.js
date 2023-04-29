c0ed4ff49e5a0f26590923dc393c0a942fce7997
vascabarkapa <vascabarkapa@gmail.com>
2023-04-29 19:31:17 +0200

    fixed navigation config

--- frontend/src/app/configs/navigationConfig.js
+++ frontend/src/app/configs/navigationConfig.js
@@ -1,42 +1,42 @@
 const navigationConfig = [
   {
-    id: 'example-component',
+    id: 'dashboard',
     title: 'Dashboard',
     type: 'item',
     icon: 'heroicons-outline:home',
-    url: 'example',
+    url: '/dashboard',
   },
   {
     id: 'temparature-component',
     title: 'Temparature',
     type: 'item',
     icon: 'heroicons-outline:sun',
-    url: '#',
+    url: '/temparature',
   },
   {
     id: 'humidity-component',
     title: 'Humidity',
     type: 'item',
     icon: 'heroicons-outline:cloud',
-    url: '#',
+    url: '/humidity',
   },
   {
     id: 'divider-1',
     type: 'divider',
   },
   {
-    id: 'users-component',
+    id: 'user-component',
     title: 'Users',
     type: 'item',
     icon: 'heroicons-outline:users',
-    url: '#',
+    url: '/user',
   },
   {
     id: 'about-component',
     title: 'About',
     type: 'item',
     icon: 'heroicons-outline:information-circle',
-    url: '#',
+    url: '/about',
   },
 ];
 
