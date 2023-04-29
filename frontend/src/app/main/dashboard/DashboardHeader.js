4a15ba1cf7cbb5b6bd5da12a308db26e1dd3b1ef
vascabarkapa <vascabarkapa@gmail.com>
2023-04-30 00:47:46 +0200

    dashboard header

---
+++ frontend/src/app/main/dashboard/DashboardHeader.js
@@ -0,0 +1,56 @@
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { useEffect, useState } from 'react';
+
+function DashboardHeader(props) {
+  const date = new Date();
+  const hours = date.getHours();
+
+  const [greeting, setGreeting] = useState();
+
+  useEffect(() => {
+    if (hours >= 3 && hours < 12) {
+      setGreeting("Good morning");
+    } else if (hours >= 12 && hours < 17) {
+      setGreeting("Good afternoon");
+    } else {
+      setGreeting("Good evening");
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col w-full px-24 sm:px-32">
+      <div className="flex flex-col sm:flex-row flex-auto sm:items-center min-w-0 my-32 sm:my-48">
+        <div className="flex flex-auto items-center min-w-0">
+          <div className="flex flex-col min-w-0 mx-16">
+            <Typography className="text-2xl md:text-5xl font-semibold tracking-tight leading-7 md:leading-snug truncate">
+              {`${greeting}, Test User!`}
+            </Typography>
+
+            <div className="flex items-center">
+              <FuseSvgIcon size={20} color="action">
+                heroicons-outline:chip
+              </FuseSvgIcon>
+              <Typography className="mx-6 leading-6 truncate" color="text.secondary">
+                Take control of your surroundings with eRegulation
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12">
+          <Button
+            className="whitespace-nowrap"
+            variant="contained"
+            color="secondary"
+            startIcon={<FuseSvgIcon size={20}>heroicons-solid:cog</FuseSvgIcon>}
+          >
+            Settings
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardHeader;
