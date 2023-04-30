26c4bb0b146d946df45b9e804a3b49d54220bbe3
vascabarkapa <vascabarkapa@gmail.com>
2023-05-01 01:11:15 +0200

    users table

---
+++ frontend/src/app/main/users/UsersTable.js
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Button, Tooltip } from '@mui/material';
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon/FuseSvgIcon';
+import { motion } from 'framer-motion';
+
+function createData(first_name, last_name, username, email, actions) {
+    return { first_name, last_name, username, email, actions };
+}
+
+const rows = [
+    createData('John', 'Smith', 'johnsmith', 'john.smith@gmail.com', ''),
+    createData('Emma', 'Johnson', 'emmajohnson', 'emma.johnson@gmail.com', ''),
+    createData('Michael', 'Davis', 'michaeldavis', 'michael.davis@gmail.com', ''),
+    createData('Sophia', 'Garcia', 'sophiagarcia', 'sophia.garcia@gmail.com', ''),
+    createData('William', 'Brown', 'williambrown', 'william.brown@gmail.com', ''),
+    createData('Isabella', 'Martinez', 'isabellamartinez', 'isabella.martinez@gmail.com', ''),
+    createData('Ethan', 'Anderson', 'ethananderson', 'ethan.anderson@gmail.com', ''),
+    createData('Olivia', 'Taylor', 'oliviataylor', 'olivia.taylor@gmail.com', ''),
+    createData('Liam', 'Thomas', 'liamthomas', 'liam.thomas@gmail.com', ''),
+    createData('Ava', 'Hernandez', 'avahernandez', 'ava.hernandez@gmail.com', ''),
+];
+
+export default function UsersPage() {
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+        >
+            <TableContainer sx={{ width: '95%', marginLeft: 'auto', marginRight: 'auto', marginBottom: '2rem' }} component={Paper}>
+                <Table sx={{ minWidth: 650 }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Full Name</TableCell>
+                            <TableCell>Username</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell>{row.first_name + ' ' + row.last_name}</TableCell>
+                                <TableCell>{row.username}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                                <TableCell align="right">
+                                    <Tooltip title="Edit" placement="top">
+                                        <Button
+                                            variant="text"
+                                            color="secondary"
+                                            type="button"
+                                        >
+                                            <FuseSvgIcon>
+                                                heroicons-solid:pencil-alt
+                                            </FuseSvgIcon>
+                                        </Button>
+                                    </Tooltip>
+                                    <Tooltip title="Delete" placement="top">
+                                        <Button
+                                            variant="text"
+                                            color="secondary"
+                                            type="button"
+                                        >
+                                            <FuseSvgIcon>
+                                                heroicons-solid:trash
+                                            </FuseSvgIcon>
+                                        </Button>
+                                    </Tooltip>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </motion.div>
+    );
+}
