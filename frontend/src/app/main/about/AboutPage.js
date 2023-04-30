6750bd4712a7e3f083740ab3b813864fb959813d
vascabarkapa <vascabarkapa@gmail.com>
2023-04-30 23:15:09 +0200

    layout of about page

--- frontend/src/app/main/about/AboutPage.js
+++ frontend/src/app/main/about/AboutPage.js
@@ -4,12 +4,12 @@ import { motion } from 'framer-motion';
 function AboutPage() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center p-16">
-            <div className="w-full max-w-6xl text-center mb-20">
+            <div className="w-full max-w-6xl text-center mb-72 sm:mb-20">
                 <motion.div
                     initial={{ opacity: 0, y: 40 }}
                     animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
                 >
-                    <img className="mx-auto w-2/5 mb-20 animate-pulse" src="assets/images/logo/eregulation-light.svg" alt="logo" />
+                    <img className="mx-auto w-3/5 sm:w-2/5 mb-20 animate-pulse" src="assets/images/logo/eregulation-light.svg" alt="logo" />
                     <Typography
                         variant="h1"
                         className="text-4xl md:text-6xl font-extrabold tracking-tight leading-tight md:leading-none text-center mb-10"
@@ -26,9 +26,12 @@ function AboutPage() {
                 </motion.div>
 
             </div>
-            <div className="fixed bottom-0 p-4 pb-20 text-center">
+            <Typography
+                variant="h7"
+                className="fixed bottom-0 p-4 pb-20 text-center font-bold"
+            >
                 University of East Sarajevo, Faculty of Electrical Engineering &copy; 2023
-            </div>
+            </Typography>
         </div>
     );
 }
