abf6aa08b45905c38cd70fa3866db392f76c44bd
vascabarkapa <vascabarkapa@gmail.com>
2023-05-01 23:43:12 +0200

    animate live data

--- frontend/src/app/main/humidity/HumidityLive.js
+++ frontend/src/app/main/humidity/HumidityLive.js
@@ -4,40 +4,46 @@ import Typography from '@mui/material/Typography';
 import { memo } from 'react';
 import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
 import DateTimeHelper from 'src/app/shared/helpers/DateTimeHelper';
+import { motion } from 'framer-motion';
 
 function HumidityLive() {
     const { date } = { date: '2023-05-01T22:24:15.000Z' };
 
     return (
-        <Paper className="relative flex flex-col flex-auto p-24 pr-12 pb-12 rounded-2xl shadow overflow-hidden">
-            <div className="flex items-center justify-between">
-                <div className="flex flex-col">
-                    <Typography className="text-3xl font-bold" variant="h1">
-                        Current Humidity
-                    </Typography>
-                    <Typography className="text-green-600 font-medium text-sm">Last Update in {DateTimeHelper.convertToLocalFormatWithSeconds(date)}</Typography>
+        <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+        >
+            <Paper className="relative flex flex-col flex-auto p-24 pr-12 pb-12 rounded-2xl shadow overflow-hidden">
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col">
+                        <Typography className="text-3xl font-bold" variant="h1">
+                            Current Humidity
+                        </Typography>
+                        <Typography className="text-green-600 font-medium text-sm">Last Update in {DateTimeHelper.convertToLocalFormatWithSeconds(date)}</Typography>
 
+                    </div>
+                    <div className="-mt-8">
+                        <IconButton aria-label="more" size="large">
+                            <FuseSvgIcon>material-outline:refresh</FuseSvgIcon>
+                        </IconButton>
+                    </div>
                 </div>
-                <div className="-mt-8">
-                    <IconButton aria-label="more" size="large">
-                        <FuseSvgIcon>material-outline:refresh</FuseSvgIcon>
-                    </IconButton>
+                <div className="flex flex-col flex-wrap mt-16">
+                    <div className="flex items-center">
+                        <div className="h-16 w-16 bg-red-500 rounded-full animate-ping mr-32"></div>
+                        <Typography className="text-9xl font-extrabold" variant="h1">
+                            48%
+                        </Typography>
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-col flex-wrap mt-16">
-                <div className="flex items-center">
-                    <div className="h-16 w-16 bg-red-500 rounded-full animate-ping mr-32"></div>
-                    <Typography className="text-9xl font-extrabold" variant="h1">
-                        48%
-                    </Typography>
+                <div className="absolute bottom-0 ltr:right-0 rtl:left-0 w-96 h-96 -m-24">
+                    <FuseSvgIcon size={100} className="opacity-25 text-white-400">
+                        material-outline:water_drop
+                    </FuseSvgIcon>
                 </div>
-            </div>
-            <div className="absolute bottom-0 ltr:right-0 rtl:left-0 w-96 h-96 -m-24">
-                <FuseSvgIcon size={100} className="opacity-25 text-white-400">
-                    material-outline:water_drop
-                </FuseSvgIcon>
-            </div>
-        </Paper>
+            </Paper>
+        </motion.div>
     );
 }
 
