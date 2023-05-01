01c9f198a1c515cde1cc47567f231e36a0429d96
vascabarkapa <vascabarkapa@gmail.com>
2023-05-01 13:58:56 +0200

    users delete modal

---
+++ frontend/src/app/main/users/UsersDeleteModal.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const UsersDeleteModal = ({ open, setOpen, onConfirm }) => {
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-delete-title"
+                aria-describedby="alert-delete-description"
+            >
+                <DialogTitle id="alert-delete-title">
+                    Delete confirmation
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-delete-description">
+                        Are you sure you want to delete the User?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button color="error" onClick={onConfirm}>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default UsersDeleteModal;
