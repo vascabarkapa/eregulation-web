487d924f43a2e2220aff6ec05cb049061a6922de
vascabarkapa <vascabarkapa@gmail.com>
2023-05-01 23:40:12 +0200

    us format datetime

--- frontend/src/app/shared/helpers/DateTimeHelper.js
+++ frontend/src/app/shared/helpers/DateTimeHelper.js
@@ -1,41 +1,89 @@
 function convertToLocalFormatWithSeconds(dateTime) {
-    const dateObj = new Date(dateTime);
+    const date = new Date(dateTime);
 
-    // Format date in 24-hour format
-    const hours = dateObj.getHours().toString().padStart(2, '0');
-    const minutes = dateObj.getMinutes().toString().padStart(2, '0');
-    const seconds = dateObj.getSeconds().toString().padStart(2, '0');
-    const formattedTime = `${hours}:${minutes}:${seconds}`;
+    const month = date.getMonth() + 1; // getMonth() returns 0-based month, so add 1
+    const day = date.getDate();
+    const year = date.getFullYear();
 
-    // Format date in DD.MM.YYYY format
-    const day = dateObj.getDate().toString().padStart(2, '0');
-    const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
-    const year = dateObj.getFullYear();
-    const formattedDate = `${day}.${month}.${year}`;
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
-    return formattedDate + " " + formattedTime;
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12; // convert 24-hour to 12-hour format
+    const paddedMonth = month.toString().padStart(2, '0'); // pad month with leading zero if needed
+    const paddedDay = day.toString().padStart(2, '0'); // pad day with leading zero if needed
+
+    return `${paddedMonth}/${paddedDay}/${year} ${formattedHours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
 }
 
 function convertToLocalFormat(dateTime) {
-    const dateObj = new Date(dateTime);
+    const date = new Date(dateTime);
 
-    // Format date in 24-hour format
-    const hours = dateObj.getHours().toString().padStart(2, '0');
-    const minutes = dateObj.getMinutes().toString().padStart(2, '0');
-    const formattedTime = `${hours}:${minutes}h`;
+    const month = date.getMonth() + 1; // getMonth() returns 0-based month, so add 1
+    const day = date.getDate();
+    const year = date.getFullYear();
 
-    // Format date in DD.MM.YYYY format
-    const day = dateObj.getDate().toString().padStart(2, '0');
-    const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
-    const year = dateObj.getFullYear();
-    const formattedDate = `${day}.${month}.${year}`;
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
 
-    return formattedDate + " " + formattedTime;
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12; // convert 24-hour to 12-hour format
+    const paddedMonth = month.toString().padStart(2, '0'); // pad month with leading zero if needed
+    const paddedDay = day.toString().padStart(2, '0'); // pad day with leading zero if needed
+
+    return `${paddedMonth}/${paddedDay}/${year} ${formattedHours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
 }
+
+function getDate(dateTime) {
+    const date = new Date(dateTime);
+
+    const month = date.getMonth() + 1; // getMonth() returns 0-based month, so add 1
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    const paddedMonth = month.toString().padStart(2, '0'); // pad month with leading zero if needed
+    const paddedDay = day.toString().padStart(2, '0'); // pad day with leading zero if needed
+
+    return `${paddedMonth}/${paddedDay}/${year}`;
+}
+
+function getTime(dateTime) {
+    const date = new Date(dateTime);
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12; // convert 24-hour to 12-hour format
+
+    return `${formattedHours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+}
+
+function getTimeWithSeconds(dateTime) {
+    const date = new Date(dateTime);
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12; // convert 24-hour to 12-hour format
+
+    return `${formattedHours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
+}
+
 
 const DateTimeHelper = {
     convertToLocalFormatWithSeconds,
-    convertToLocalFormat
+    convertToLocalFormat,
+    getDate,
+    getTime,
+    getTimeWithSeconds
 }
 
 export default DateTimeHelper;
