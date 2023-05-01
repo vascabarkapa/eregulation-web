a6d73abf0d694b8a3572d123272b617d1bc92007
vascabarkapa <vascabarkapa@gmail.com>
2023-05-01 21:05:37 +0200

    temparature header

---
+++ frontend/src/app/main/temparature/TemparatureHeader.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+
+const TemparatureHeader = () => {
+    return (
+        <>
+            <div className="flex flex-col sm:flex-row space-y-16 sm:space-y-0 w-full items-center justify-between py-32  px-24 sm:px-32">
+                <Typography
+                    component={motion.span}
+                    initial={{ x: -20 }}
+                    animate={{ x: 0, transition: { delay: 0.2 } }}
+                    delay={300}
+                    className="text-24 md:text-32 font-extrabold tracking-tight mx-16"
+                >
+                    Temparature
+                </Typography>
+            </div>
+        </>
+
+    );
+}
+
+export default TemparatureHeader;
