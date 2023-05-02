4f40e48720df7de93e7902bcca28b952c4f4f989
vascabarkapa <vascabarkapa@gmail.com>
2023-05-02 23:34:50 +0200

    added temparature average chart

--- frontend/src/app/main/temparature/TemparaturePage.js
+++ frontend/src/app/main/temparature/TemparaturePage.js
@@ -2,12 +2,13 @@ import DateRangeFilter from "src/app/shared/components/DateRangeFilter";
 import TemparatureHeader from "./TemparatureHeader";
 import TemparatureLive from "./TemparatureLive";
 import TemparatureTable from "./TemparatureTable";
+import TemperatureAverageChart from "./TemparatureAverageChart";
 
 const TemparaturePage = () => {
     return (
         <div className="container">
             <TemparatureHeader />
-            <div className="hidden md:flex flex-col md:flex-row md:justify-between gap-20 mx-20">
+            <div className="hidden md:flex flex-col md:flex-row md:justify-between gap-20 mx-20 mb-20">
                 <div className="w-full md:w-1/2">
                     <TemparatureTable />
                 </div>
@@ -18,10 +19,17 @@ const TemparaturePage = () => {
                     </div>
                 </div>
             </div>
+            <div className="hidden md:flex flex-col md:flex-row md:justify-between gap-20 mx-20 mb-20">
+                <div className="w-full">
+                    <TemperatureAverageChart />
+                </div>
+            </div>
+
             <div className="flex flex-col gap-20 md:hidden m-20">
                 <TemparatureLive />
                 <DateRangeFilter />
                 <TemparatureTable />
+                <TemperatureAverageChart />
             </div>
         </div>
     );
