e23689ac20ccd130f136955c3e0b7955a30ed378
vascabarkapa <vascabarkapa@gmail.com>
2023-05-03 01:23:59 +0200

    my profile modal

---
+++ frontend/src/app/shared/components/MyProfileModal.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const MyProfileModal = ({ open, setOpen }) => {
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-users-delete-title"
+                aria-describedby="alert-users-delete-description"
+            >
+                <DialogTitle id="alert-users-delete-title">
+                    Delete confirmation
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-users-delete-description">
+                        Are you sure you want to delete the User?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default MyProfileModal;
