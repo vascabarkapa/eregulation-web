83e459f1075b2bd8e6951aef5e9ebaa9ede37878
vascabarkapa <vascabarkapa@gmail.com>
2023-05-02 23:51:26 +0200

    added statistical humidity data

---
+++ frontend/src/app/main/humidity/HumidityAverageChart.js
@@ -0,0 +1,319 @@
+import Paper from '@mui/material/Paper';
+import { lighten, useTheme } from '@mui/material/styles';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Typography from '@mui/material/Typography';
+import { memo, useEffect, useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
+import Box from '@mui/material/Box';
+
+function HumidityAverageChart() {
+    const theme = useTheme();
+    const [awaitRender, setAwaitRender] = useState(true);
+    const [tabValue, setTabValue] = useState(0);
+    const { overview, series, ranges, labels } = {
+        "overview": {
+            "this-week": {
+                "max-humidity": 28,
+                "min-humidity": 12,
+                "fixed": 3,
+                "wont-fix": 4,
+                "re-opened": 8,
+                "needs-triage": 6
+            },
+            "last-week": {
+                "max-humidity": 26,
+                "min-humidity": 14,
+                "fixed": 6,
+                "wont-fix": 11,
+                "re-opened": 6,
+                "needs-triage": 5
+            }
+        },
+        "ranges": {
+            "this-week": "This Week",
+            "last-week": "Last Week"
+        },
+        "labels": [
+            "Mon",
+            "Tue",
+            "Wed",
+            "Thu",
+            "Fri",
+            "Sat",
+            "Sun"
+        ],
+        "series": {
+            "this-week": [
+                {
+                    "name": "New issues",
+                    "type": "line",
+                    "data": [
+                        42,
+                        28,
+                        43,
+                        34,
+                        20,
+                        25,
+                        22
+                    ]
+                },
+                {
+                    "name": "Closed issues",
+                    "type": "column",
+                    "data": [
+                        11,
+                        10,
+                        8,
+                        11,
+                        8,
+                        10,
+                        17
+                    ]
+                }
+            ],
+            "last-week": [
+                {
+                    "name": "New issues",
+                    "type": "line",
+                    "data": [
+                        37,
+                        32,
+                        39,
+                        27,
+                        18,
+                        24,
+                        20
+                    ]
+                },
+                {
+                    "name": "Closed issues",
+                    "type": "column",
+                    "data": [
+                        9,
+                        8,
+                        10,
+                        12,
+                        7,
+                        11,
+                        15
+                    ]
+                }
+            ]
+        }
+    };
+    const currentRange = Object.keys(ranges)[tabValue];
+
+    const chartOptions = {
+        chart: {
+            fontFamily: 'inherit',
+            foreColor: 'inherit',
+            height: '100%',
+            type: 'line',
+            toolbar: {
+                show: false,
+            },
+            zoom: {
+                enabled: false,
+            },
+        },
+        colors: [theme.palette.primary.main, theme.palette.secondary.main],
+        labels,
+        dataLabels: {
+            enabled: true,
+            enabledOnSeries: [0],
+            background: {
+                borderWidth: 0,
+            },
+        },
+        grid: {
+            borderColor: theme.palette.divider,
+        },
+        legend: {
+            show: false,
+        },
+        plotOptions: {
+            bar: {
+                columnWidth: '50%',
+            },
+        },
+        states: {
+            hover: {
+                filter: {
+                    type: 'darken',
+                    value: 0.75,
+                },
+            },
+        },
+        stroke: {
+            width: [3, 0],
+        },
+        tooltip: {
+            followCursor: true,
+            theme: theme.palette.mode,
+        },
+        xaxis: {
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                color: theme.palette.divider,
+            },
+            labels: {
+                style: {
+                    colors: theme.palette.text.secondary,
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+        },
+        yaxis: {
+            labels: {
+                offsetX: -16,
+                style: {
+                    colors: theme.palette.text.secondary,
+                },
+            },
+        },
+    };
+
+    useEffect(() => {
+        setAwaitRender(false);
+    }, []);
+
+    if (awaitRender) {
+        return null;
+    }
+
+    return (
+        <Paper className="flex flex-col flex-auto p-24 shadow rounded-2xl overflow-hidden">
+            <div className="flex flex-col sm:flex-row items-start justify-between">
+                <Typography className="text-lg font-medium tracking-tight leading-6 truncate">
+                    Statistical Humidity Data
+                </Typography>
+                <div className="mt-12 sm:mt-0 sm:ml-8">
+                    <Tabs
+                        value={tabValue}
+                        onChange={(ev, value) => setTabValue(value)}
+                        indicatorColor="secondary"
+                        textColor="inherit"
+                        variant="scrollable"
+                        scrollButtons={false}
+                        className="-mx-4 min-h-40"
+                        classes={{ indicator: 'flex justify-center bg-transparent w-full h-full' }}
+                        TabIndicatorProps={{
+                            children: (
+                                <Box
+                                    sx={{ bgcolor: 'text.disabled' }}
+                                    className="w-full h-full rounded-full opacity-20"
+                                />
+                            ),
+                        }}
+                    >
+                        {Object.entries(ranges).map(([key, label]) => (
+                            <Tab
+                                className="text-14 font-semibold min-h-40 min-w-64 mx-4 px-12"
+                                disableRipple
+                                key={key}
+                                label={label}
+                            />
+                        ))}
+                    </Tabs>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 grid-flow-row gap-24 w-full mt-32 sm:mt-16">
+                <div className="flex flex-col flex-auto">
+                    <Typography className="font-medium" color="text.secondary">
+                        Average Humidity
+                    </Typography>
+                    <div className="flex flex-col flex-auto">
+                        <ReactApexChart
+                            className="flex-auto w-full"
+                            options={chartOptions}
+                            series={series[currentRange]}
+                            height={320}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <Typography className="font-medium" color="text.secondary">
+                        Overview
+                    </Typography>
+                    <div className="flex-auto grid grid-cols-4 gap-16 mt-24">
+                        <div className="col-span-2 flex flex-col items-center justify-center py-32 px-4 rounded-2xl bg-indigo-50 text-indigo-800">
+                            <Typography className="text-5xl sm:text-7xl font-semibold leading-none tracking-tight">
+                                {overview[currentRange]['max-humidity']}
+                            </Typography>
+                            <Typography className="mt-4 text-sm sm:text-lg font-medium">Max Humidity</Typography>
+                        </div>
+                        <div className="col-span-2 flex flex-col items-center justify-center py-32 px-4 rounded-2xl bg-green-50 text-green-800">
+                            <Typography className="text-5xl sm:text-7xl font-semibold leading-none tracking-tight">
+                                {overview[currentRange]['min-humidity']}
+                            </Typography>
+                            <Typography className="mt-4 text-sm sm:text-lg font-medium">Min Humidity</Typography>
+                        </div>
+                        <Box
+                            sx={{
+                                backgroundColor: (_theme) =>
+                                    _theme.palette.mode === 'light'
+                                        ? lighten(theme.palette.background.default, 0.4)
+                                        : lighten(theme.palette.background.default, 0.02),
+                            }}
+                            className="col-span-2 sm:col-span-1 flex flex-col items-center justify-center py-32 px-4 rounded-2xl"
+                        >
+                            <Typography className="text-5xl font-semibold leading-none tracking-tight">
+                                {overview[currentRange].fixed}
+                            </Typography>
+                            <Typography className="mt-4 text-sm font-medium text-center">Fixed</Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                backgroundColor: (_theme) =>
+                                    _theme.palette.mode === 'light'
+                                        ? lighten(theme.palette.background.default, 0.4)
+                                        : lighten(theme.palette.background.default, 0.02),
+                            }}
+                            className="col-span-2 sm:col-span-1 flex flex-col items-center justify-center py-32 px-4 rounded-2xl"
+                        >
+                            <Typography className="text-5xl font-semibold leading-none tracking-tight">
+                                {overview[currentRange]['wont-fix']}
+                            </Typography>
+                            <Typography className="mt-4 text-sm font-medium text-center">Won't Fix</Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                backgroundColor: (_theme) =>
+                                    _theme.palette.mode === 'light'
+                                        ? lighten(theme.palette.background.default, 0.4)
+                                        : lighten(theme.palette.background.default, 0.02),
+                            }}
+                            className="col-span-2 sm:col-span-1 flex flex-col items-center justify-center py-32 px-4 rounded-2xl"
+                        >
+                            <Typography className="text-5xl font-semibold leading-none tracking-tight">
+                                {overview[currentRange]['re-opened']}
+                            </Typography>
+                            <Typography className="mt-4 text-sm font-medium text-center">Re-opened</Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                backgroundColor: (_theme) =>
+                                    _theme.palette.mode === 'light'
+                                        ? lighten(theme.palette.background.default, 0.4)
+                                        : lighten(theme.palette.background.default, 0.02),
+                            }}
+                            className="col-span-2 sm:col-span-1 flex flex-col items-center justify-center py-32 px-4 rounded-2xl"
+                        >
+                            <Typography className="text-5xl font-semibold leading-none tracking-tight">
+                                {overview[currentRange]['needs-triage']}
+                            </Typography>
+                            <Typography className="mt-4 text-sm font-medium text-center">Needs Triage</Typography>
+                        </Box>
+                    </div>
+                </div>
+            </div>
+        </Paper>
+    );
+}
+
+export default memo(HumidityAverageChart);
--- frontend/src/app/main/humidity/HumidityPage.js
+++ frontend/src/app/main/humidity/HumidityPage.js
@@ -2,12 +2,13 @@ import DateRangeFilter from "src/app/shared/components/DateRangeFilter";
 import HumidityHeader from "./HumidityHeader";
 import HumidityLive from "./HumidityLive";
 import HumidityTable from "./HumidityTable";
+import HumidityAverageChart from "./HumidityAverageChart";
 
 const HumidityPage = () => {
     return (
         <div className="container">
             <HumidityHeader />
-            <div className="hidden md:flex flex-col md:flex-row md:justify-between gap-20 mx-20">
+            <div className="hidden md:flex flex-col md:flex-row md:justify-between gap-20 mx-20 mb-20">
                 <div className="w-full md:w-1/2">
                     <HumidityTable />
                 </div>
@@ -18,10 +19,17 @@ const HumidityPage = () => {
                     </div>
                 </div>
             </div>
+            <div className="hidden md:flex flex-col md:flex-row md:justify-between gap-20 mx-20 mb-20">
+                <div className="w-full">
+                    <HumidityAverageChart />
+                </div>
+            </div>
+
             <div className="flex flex-col gap-20 md:hidden m-20">
                 <HumidityLive />
                 <DateRangeFilter />
                 <HumidityTable />
+                <HumidityAverageChart />
             </div>
         </div>
     );
