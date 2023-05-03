7f07ff0c2afd33bb46bccfb6a27dd909e3f66c7a
vascabarkapa <vascabarkapa@gmail.com>
2023-05-04 01:22:30 +0200

    dashboard header motion

--- frontend/src/app/main/dashboard/DashboardHeader.js
+++ frontend/src/app/main/dashboard/DashboardHeader.js
@@ -2,6 +2,8 @@ import Typography from '@mui/material/Typography';
 import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
 import { useEffect, useState } from 'react';
 import CurrentDateAndTime from '../../shared/components/CurrentDateAndTime';
+import { motion } from 'framer-motion';
+
 
 function DashboardHeader(props) {
   const date = new Date();
@@ -24,23 +26,39 @@ function DashboardHeader(props) {
       <div className="flex flex-col sm:flex-row flex-auto sm:items-center min-w-0 my-32 sm:my-48">
         <div className="flex flex-auto items-center min-w-0">
           <div className="flex flex-col min-w-0 mx-16">
-            <Typography className="text-2xl md:text-5xl font-semibold tracking-tight leading-7 md:leading-snug truncate">
+            <Typography className="text-2xl md:text-5xl font-semibold tracking-tight leading-7 md:leading-snug truncate"
+              component={motion.span}
+              initial={{ x: -20 }}
+              animate={{ x: 0, transition: { delay: 0.2 } }}
+              delay={300}
+            >
               {greeting}
             </Typography>
 
-            <div className="flex items-center">
-              <FuseSvgIcon size={20} color="action">
-                heroicons-outline:chip
-              </FuseSvgIcon>
-              <Typography className="mx-6 leading-6 truncate" color="text.secondary">
-                Take control of your surroundings with eRegulation
-              </Typography>
-            </div>
+            <motion.div
+              initial={{ x: -20 }}
+              animate={{ x: 0, transition: { delay: 0.2 } }}
+            >
+              <div className="flex items-center">
+                <FuseSvgIcon size={20} color="action">
+                  heroicons-outline:chip
+                </FuseSvgIcon>
+                <Typography className="mx-6 leading-6 truncate" color="text.secondary">
+                  Take control of your surroundings with eRegulation
+                </Typography>
+              </div>
+            </motion.div>
           </div>
         </div>
-        <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12 mx-16">
-          <CurrentDateAndTime />
-        </div>
+        <motion.div
+          initial={{ opacity: 0, x: 20 }}
+          animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
+          className="mx-16"
+        >
+          <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12 mx-16">
+            <CurrentDateAndTime />
+          </div>
+        </motion.div>
       </div>
     </div>
   );
