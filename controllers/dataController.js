2c3147886543aa10f4f527c476736879737a5079
vascabarkapa <vascabarkapa@gmail.com>
2023-12-19 23:39:17 +0100

    optimized data

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,8 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const Data = require("../models/dataModel");
 
-//@desc Add data from MQTT message
-//@access public
+/**
+ * @desc Add data from MQTT message
+ * @access Public
+ */
 const addData = asyncHandler(async (body) => {
     try {
         await Data.create(body);
@@ -11,32 +13,52 @@ const addData = asyncHandler(async (body) => {
     }
 });
 
-//@desc Get all temperature data
-//@route GET /api/data/temperature
-//@access private
+/**
+ * @desc Get all temperature data
+ * @route GET /api/data/temperature
+ * @access Private
+ */
 const getTemperatureData = asyncHandler(async (req, res) => {
-    const temperatureData = await Data.find({ type: "t" });
+    try {
+        const temperatureData = await Data.find({ type: "t" });
 
-    res.status(200).json(temperatureData);
+        res.status(200).json(temperatureData);
+    } catch (error) {
+        console.error("Error getting temperature data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-//@desc Get all humidity data
-//@route GET /api/data/humidity
-//@access private
+/**
+ * @desc Get all humidity data
+ * @route GET /api/data/humidity
+ * @access Private
+ */
 const getHumidityData = asyncHandler(async (req, res) => {
-    const humidityData = await Data.find({ type: "h" });
+    try {
+        const humidityData = await Data.find({ type: "h" });
 
-    res.status(200).json(humidityData);
+        res.status(200).json(humidityData);
+    } catch (error) {
+        console.error("Error getting humidity data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-//@desc Get all light data
-//@route GET /api/data/light
-//@access private
+/**
+ * @desc Get all light data
+ * @route GET /api/data/light
+ * @access Private
+ */
 const getLightData = asyncHandler(async (req, res) => {
-    const lightData = await Data.find({ type: "l" });
+    try {
+        const lightData = await Data.find({ type: "l" });
 
-    res.status(200).json(lightData);
+        res.status(200).json(lightData);
+    } catch (error) {
+        console.error("Error getting light data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-
-module.exports = {addData, getTemperatureData, getHumidityData, getLightData};
+module.exports = { addData, getTemperatureData, getHumidityData, getLightData };
