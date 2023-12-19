0695d3989635196d1edded9b4f9bd6718cd0900d
vascabarkapa <vascabarkapa@gmail.com>
2023-12-19 23:37:00 +0100

    get all data

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -11,4 +11,32 @@ const addData = asyncHandler(async (body) => {
     }
 });
 
-module.exports = {addData};
+//@desc Get all temperature data
+//@route GET /api/data/temperature
+//@access private
+const getTemperatureData = asyncHandler(async (req, res) => {
+    const temperatureData = await Data.find({ type: "t" });
+
+    res.status(200).json(temperatureData);
+});
+
+//@desc Get all humidity data
+//@route GET /api/data/humidity
+//@access private
+const getHumidityData = asyncHandler(async (req, res) => {
+    const humidityData = await Data.find({ type: "h" });
+
+    res.status(200).json(humidityData);
+});
+
+//@desc Get all light data
+//@route GET /api/data/light
+//@access private
+const getLightData = asyncHandler(async (req, res) => {
+    const lightData = await Data.find({ type: "l" });
+
+    res.status(200).json(lightData);
+});
+
+
+module.exports = {addData, getTemperatureData, getHumidityData, getLightData};
