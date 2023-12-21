60e8a2d9bf6c8df1248ba2f2587708ac4a1c9fc0
vascabarkapa <vascabarkapa@gmail.com>
2023-12-22 00:24:45 +0100

    updated cronjob

--- config/cronjob.js
+++ config/cronjob.js
@@ -1,6 +1,17 @@
+const axios = require('axios');
 const cron = require('node-cron');
+const dotenv = require("dotenv").config();
 
-cron.schedule('*/5 * * * * *', () => {
-    console.log('Last live data retrieved from MQTT broker: ' + new Date());
-
+cron.schedule('*/30 * * * * *', () => {
+    getLatestData();
 });
+
+function getLatestData() {
+    axios.get(process.env.API_URL + 'data/latest')
+        .then((response) => {
+            console.log('Last live data retrieved from MQTT broker: ' + new Date());
+        })
+        .catch((error) => {
+            console.error('Error calling API:', error.message);
+        });
+}
