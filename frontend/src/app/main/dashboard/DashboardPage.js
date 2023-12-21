2cd1a5e611bce106973a0e1ff99bc8971ac1f594
vascabarkapa <vascabarkapa@gmail.com>
2023-12-21 23:26:32 +0100

    added notification

--- frontend/src/app/main/dashboard/DashboardPage.js
+++ frontend/src/app/main/dashboard/DashboardPage.js
@@ -1,6 +1,5 @@
 import DashboardHeader from "./components/DashboardHeader";
 import FusePageSimple from '@fuse/core/FusePageSimple';
-import _ from '@lodash';
 import {styled} from '@mui/material/styles';
 import DashboardTemperatureHistoryChart from "./components/DashboardTemperatureHistoryChart";
 import DashboardHumidityHistoryChart from "./components/DashboardHumidityHistoryChart";
@@ -10,6 +9,10 @@ import DashboardLightLive from "./components/DashboardLightLive";
 import {useEffect, useState} from "react";
 import DataService from "../../shared/services/data-service";
 import LightDataHelper from "../../shared/helpers/LightDataHelper";
+import FuseLoading from "@fuse/core/FuseLoading";
+import {useDispatch} from 'react-redux';
+import {showMessage} from "app/store/fuse/messageSlice";
+
 
 const Root = styled(FusePageSimple)(({theme}) => ({
     '& .FusePageSimple-header': {
@@ -19,6 +22,8 @@ const Root = styled(FusePageSimple)(({theme}) => ({
 }));
 
 const DashboardPage = () => {
+    const dispatch = useDispatch();
+
     const [trigger, setTrigger] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [liveTemperature, setLiveTemperature] = useState({});
@@ -26,52 +31,37 @@ const DashboardPage = () => {
     const [liveLight, setLiveLight] = useState({});
 
     useEffect(() => {
-        DataService.getLatestData().then((response) => {
-            setIsLoading(true);
-            if (response) {
-                setLiveTemperature(response?.data["t"]);
-                setLiveHumidity(response?.data["h"]);
-                setLiveLight(response?.data["l"]);
+        setIsLoading(true);
 
-                const handleVisibilityChange = () => {
-                    if (document.hidden) {
-                        document.title = response?.data["t"]?.value +
-                            "\u00B0C | " + response?.data["h"]?.value + "% | " +
-                            LightDataHelper.getModeValue(response?.data["l"]?.value);
-                    } else {
-                        document.title = "eRegulation";
-                    }
-                };
+        try {
+            DataService.getLatestData().then((response) => {
+                if (response) {
+                    setLiveTemperature(response?.data["t"]);
+                    setLiveHumidity(response?.data["h"]);
+                    setLiveLight(response?.data["l"]);
 
-                document.addEventListener("visibilitychange", handleVisibilityChange);
-                setIsLoading(false);
-            }
-        })
-    }, []);
+                    const handleVisibilityChange = () => {
+                        if (document.hidden) {
+                            document.title = response?.data["t"]?.value +
+                                "\u00B0C | " + response?.data["h"]?.value + "% | " +
+                                LightDataHelper.getModeValue(response?.data["l"]?.value);
+                        } else {
+                            document.title = "eRegulation";
+                        }
+                    };
 
-    useEffect(() => {
-        DataService.pingLatestData().then((response) => {
-            setIsLoading(true);
-            if (response) {
-                setLiveTemperature(response?.data["t"]);
-                setLiveHumidity(response?.data["h"]);
-                setLiveLight(response?.data["l"]);
-
-                const handleVisibilityChange = () => {
-                    if (document.hidden) {
-                        document.title = response?.data["t"]?.value +
-                            "\u00B0C | " + response?.data["h"]?.value + "% | " +
-                            LightDataHelper.getModeValue(response?.data["l"]?.value);
-                    } else {
-                        document.title = "eRegulation";
-                    }
-                };
-
-                document.addEventListener("visibilitychange", handleVisibilityChange);
-                setIsLoading(false);
-            }
-        })
+                    document.addEventListener("visibilitychange", handleVisibilityChange);
+                    dispatch(showMessage({message: "Updated latest data"}));
+                    setIsLoading(false);
+                }
+            });
+        } catch (error) {
+            console.error("Error fetching latest data:", error);
+            dispatch(showMessage({message: error || "An error occurred! Reload page."}));
+            setIsLoading(false);
+        }
     }, [trigger]);
+
 
     return (
         <Root
@@ -79,18 +69,24 @@ const DashboardPage = () => {
             content={
                 <div className="grid grid-cols-1 w-full mt-10 mx-20">
                     <div className="grid grid-cols-1 md:grid-cols-3">
-                        <div className="md:col-span-1 col-span-3 my-10 md:mr-10">
-                            {!isLoading && <DashboardTemperatureLive liveTemperature={liveTemperature} trigger={trigger}
-                                                                     setTrigger={setTrigger}/>}
-                        </div>
-                        <div className="md:col-span-1 col-span-3 my-10 md:ml-10">
-                            {!isLoading && <DashboardHumidityLive liveHumidity={liveHumidity} trigger={trigger}
-                                                                  setTrigger={setTrigger}/>}
-                        </div>
-                        <div className="md:col-span-1 col-span-3 my-10 md:ml-10">
-                            {!isLoading && <DashboardLightLive liveLight={liveLight} trigger={trigger}
-                                                               setTrigger={setTrigger}/>}
-                        </div>
+                        {
+                            isLoading ?
+                                <FuseLoading/> :
+                                <>
+                                    <div className="md:col-span-1 col-span-3 my-10 md:mr-10">
+                                        <DashboardTemperatureLive liveTemperature={liveTemperature} trigger={trigger}
+                                                                  setTrigger={setTrigger}/>
+                                    </div>
+                                    <div className="md:col-span-1 col-span-3 my-10 md:ml-10">
+                                        <DashboardHumidityLive liveHumidity={liveHumidity} trigger={trigger}
+                                                               setTrigger={setTrigger}/>
+                                    </div>
+                                    <div className="md:col-span-1 col-span-3 my-10 md:ml-10">
+                                        <DashboardLightLive liveLight={liveLight} trigger={trigger}
+                                                            setTrigger={setTrigger}/>
+                                    </div>
+                                </>
+                        }
                     </div>
                     <div className="w-full my-10">
                         {/*<DashboardTemperatureHistoryChart />*/}
