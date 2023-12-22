bd4bcbd3f19fbfa4da51b02815b0325e4f5317e8
vascabarkapa <vascabarkapa@gmail.com>
2023-12-23 00:20:00 +0100

    change date format for search

--- frontend/src/app/shared/components/DateRangeFilter.js
+++ frontend/src/app/shared/components/DateRangeFilter.js
@@ -1,34 +1,20 @@
 import * as React from 'react';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TextField, Card, Grid, Typography } from '@mui/material';
-import { useState } from 'react';
-import { motion } from 'framer-motion';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import {TextField, Card, Grid, Typography} from '@mui/material';
+import {useState} from 'react';
+import {motion} from 'framer-motion';
+import DateTimeHelper from "../helpers/DateTimeHelper";
 
-const DateRangeFilter = () => {
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-
-    const handleStartDateChange = (date) => {
-        setStartDate(date);
-    };
-
-    const handleEndDateChange = (date) => {
-        if (startDate && date < startDate) {
-            setEndDate(startDate);
-        } else {
-            setEndDate(date);
-        }
-    };
-
+const DateRangeFilter = ({startDate, setStartDate, endDate, setEndDate}) => {
     return (
         <motion.div
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+            initial={{opacity: 0, y: 40}}
+            animate={{opacity: 1, y: 0, transition: {delay: 0.2}}}
         >
             <Card className='p-24'>
-                <Typography className="text-3xl font-bold" variant="h1" sx={{ mb: 2 }}>
+                <Typography className="text-3xl font-bold" variant="h1" sx={{mb: 2}}>
                     Choose a date range
                 </Typography>
                 <Grid container spacing={2}>
@@ -37,8 +23,8 @@ const DateRangeFilter = () => {
                             <DatePicker
                                 label="Start Date"
                                 value={startDate}
-                                onChange={handleStartDateChange}
-                                renderInput={(props) => <TextField {...props} fullWidth />}
+                                onChange={(value) => setStartDate(DateTimeHelper.convertToSearchDate(value))}
+                                renderInput={(props) => <TextField {...props} fullWidth/>}
                             />
                         </LocalizationProvider>
                     </Grid>
@@ -47,9 +33,8 @@ const DateRangeFilter = () => {
                             <DatePicker
                                 label="End Date"
                                 value={endDate}
-                                minDate={startDate}
-                                onChange={handleEndDateChange}
-                                renderInput={(props) => <TextField {...props} fullWidth />}
+                                onChange={(value) => setEndDate(DateTimeHelper.convertToSearchDate(value))}
+                                renderInput={(props) => <TextField {...props} fullWidth/>}
                             />
                         </LocalizationProvider>
                     </Grid>
